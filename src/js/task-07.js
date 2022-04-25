98fa6b66daeb6ab57199aac0f6cfbce2ec294172
KizievDmitriy <kizievdmitrii@gmail.com>
2022-04-25 22:59:39 +0300

    Update task

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,15 +1,9 @@
-const inputEl = document.querySelector("#font-size-control");
+const inputEl = document.querySelector('#font-size-control');
 
-const spanEl = document.querySelector("#text"); 
-
-
-inputEl.value = 16;
-
+const spanEl = document.querySelector('#text');
 
 inputEl.addEventListener('input', (event) => {
 
     spanEl.style.fontSize = `${event.currentTarget.value}px`;
-
-    console.log(event);
 
 });
